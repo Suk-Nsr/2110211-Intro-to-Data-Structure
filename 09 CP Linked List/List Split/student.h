e8edfd26074cf717aeb04a4376ef6d7c05cab940
Sukrit Nuntasakulwiroj <sukrit.ntsw@gmail.com>
2025-11-10 17:39:25 +0700

    List Split

--- 09 CP Linked List/List Split/student.h
+++ 09 CP Linked List/List Split/student.h
@@ -4,6 +4,34 @@ void splitList(list<T> &list1, list<T> &list2)
     {
         return;
     }   
+    else
+    {
+        size_t checkpoint = (this->mSize / 2);
+        auto it1 = this->begin(), it2 = this->begin();
 
-    
+        for (size_t i = 0; i < this->mSize - checkpoint; ++i)
+        {
+            ++it1;
+            ++it2;
+        }
+
+        --it1;
+
+        list1.mHeader->prev->next = this->mHeader->next;
+        this->mHeader->next->prev = list1.mHeader->prev;
+        list1.mHeader->prev = it1.ptr;
+        it1.ptr->next = list1.mHeader;
+
+        list2.mHeader->prev->next = it2.ptr;
+        it2.ptr->prev = list2.mHeader->prev;
+        list2.mHeader->prev = this->mHeader->prev;
+        this->mHeader->prev->next = list2.mHeader;
+
+        this->mHeader->next = this->mHeader;
+        this->mHeader->prev = this->mHeader;
+
+        list1.mSize += (this->mSize - (this->mSize / 2));
+        list2.mSize += this->mSize / 2;
+        this->mSize = 0;
+    }
 }
