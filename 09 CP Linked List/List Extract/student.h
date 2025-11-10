ce6e2487713debdfdbd67b2ab08ceb4ed511f0e1
Sukrit Nuntasakulwiroj <sukrit.ntsw@gmail.com>
2025-11-10 19:49:11 +0700

    List Extract

---
+++ 09 CP Linked List/List Extract/student.h
@@ -0,0 +1,28 @@
+#ifndef __STUDENT_H_
+#define __STUDENT_H_
+
+#include <algorithm>
+
+template <typename T>
+void CP::list<T>::extract(const T &value, iterator a, iterator b, CP::list<T> &output)
+{
+  auto it = a;
+  while (it != b)
+  {
+    auto nextNode(it.ptr->next);
+    if (it.ptr->data == value)
+    {
+      it.ptr->prev->next = it.ptr->next;
+      it.ptr->next->prev = it.ptr->prev;
+      output.mHeader->next->prev = it.ptr;
+      it.ptr->next = output.mHeader->next;
+      output.mHeader->next = it.ptr;
+      it.ptr->prev = output.mHeader;
+      output.mSize++;
+      this->mSize--;
+    }
+    it = nextNode;
+  }
+}
+
+#endif
