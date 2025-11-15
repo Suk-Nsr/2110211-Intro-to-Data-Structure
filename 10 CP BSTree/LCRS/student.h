1b5fae42789a3b67f2ff07ef52fe8f1092e4209a
Sukrit Nuntasakulwiroj <sukrit.ntsw@gmail.com>
2025-11-15 14:34:24 +0700

    Left Child Right Sibling

--- 10 CP BSTree/LCRS/student.h
+++ 10 CP BSTree/LCRS/student.h
@@ -1,6 +1,20 @@
-// You may include library here
-
 int depth(node *n)
 {
-  // Write Code Here
+  if (n == NULL)
+  {
+    return -1;
+  }
+  else if (n->left_child == NULL && n->right_sibling == NULL)
+  {
+    return 0;
+  }
+  else if (n->left_child == NULL)
+  {
+    return depth(n->right_sibling);
+  }
+  else if (n->right_sibling == NULL)
+  {
+    return depth(n->left_child) + 1;
+  }
+  return std::max(depth(n->left_child) + 1, depth(n->right_sibling));
 }
