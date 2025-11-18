5074e55106a5d18c4708ca44e0733c8e1ae5a419
Sukrit Nuntasakulwiroj <sukrit.ntsw@gmail.com>
2025-11-18 11:30:15 +0700

    List Block Swap 70/100

--- 09 CP Linked List/List Block Swap/student.h
+++ 09 CP Linked List/List Block Swap/student.h
@@ -10,14 +10,10 @@ void CP::list<T>::block_swap(iterator a1, iterator a2, iterator b1, iterator b2)
 	{
 		return;
 	}
-	a1.ptr->prev->next = b1.ptr;
-	a1.ptr->prev = b1.ptr->prev;
-	a2.ptr->prev->next = b2.ptr;
-	a2.ptr->prev = b2.ptr->prev;
-	b1.ptr->prev->next = a1.ptr;
-	b1.ptr->prev = a1.ptr->prev;
-	b2.ptr->prev->next = a2.ptr;
-	b2.ptr->prev = a2.ptr->prev;
+	std::swap(a1.ptr->prev->next, b1.ptr->prev->next);
+	std::swap(a1.ptr->prev, b1.ptr->prev);
+	std::swap(a2.ptr->prev->next, b2.ptr->prev->next);
+	std::swap(a2.ptr->prev, b2.ptr->prev);
 }
 
 #endif
