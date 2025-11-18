f9dd2e9eeb2e65ad75227cd44511f3cd17a1d555
Sukrit Nuntasakulwiroj <sukrit.ntsw@gmail.com>
2025-11-18 17:28:47 +0700

    Same Tree nd try

--- 10 CP BSTree/Same Tree/tree.h
+++ 10 CP BSTree/Same Tree/tree.h
@@ -1,64 +1,90 @@
 #ifndef TREE_H_INCLUDED
 #define TREE_H_INCLUDED
 
-class Tree{
-    class Node{
-        public:
-            friend class Tree;
-            Node() {data = -1; left = NULL; right = NULL;}
-            Node(const int x, Node* left, Node* right) : data(x), left(left), right(right) {}
+class Tree
+{
+    class Node
+    {
+    public:
+        friend class Tree;
+        Node()
+        {
+            data = -1;
+            left = NULL;
+            right = NULL;
+        }
+        Node(const int x, Node *left, Node *right) : data(x), left(left), right(right) {}
 
-        protected:
-            int data;
-            Node* left;
-            Node* right;
+    protected:
+        int data;
+        Node *left;
+        Node *right;
     };
-public:
 
-    Tree() {
+public:
+    Tree()
+    {
         mRoot = NULL;
         mSize = 0;
     }
 
-    ~Tree() {
+    ~Tree()
+    {
         clear(mRoot);
     }
 
-    void clear(Node*& r) {
-        if (!r) return;
+    void clear(Node *&r)
+    {
+        if (!r)
+            return;
         clear(r->left);
         clear(r->right);
         delete r;
     }
 
-    void insert(int x) {
+    void insert(int x)
+    {
         insertAt(mRoot, x);
     }
-    bool isSameBinaryTree(Tree& t) {
-        return isSameNode(mRoot, t.mRoot);
+    bool isSameBinaryTree(Tree &t)
+    {
+        return isSameNode(this->mRoot, t.mRoot);
     }
     bool isSameNode(Node *a, Node *b)
     {
-        if(a == NULL && b == NULL) return true;
-        if(a == NULL || b == NULL) return false;
-        if(a->data != b->data) return false;
+        if (a == nullptr && b == nullptr)
+        {
+            return true;
+        }
+        if (a == nullptr || b == nullptr)
+        {
+            return false;
+        }
+        if (a->data != b->data)
+        {
+            return false;
+        }
         return isSameNode(a->left, b->left) && isSameNode(a->right, b->right);
     }
 
 protected:
-    void insertAt(Node*& r, int x) {
-        if (!r) {
-           r = new Node(x, NULL, NULL);
-           mSize++;
-           return;
+    void insertAt(Node *&r, int x)
+    {
+        if (!r)
+        {
+            r = new Node(x, NULL, NULL);
+            mSize++;
+            return;
         }
-        if (r->data == x) return; // Do nothing
-        if (x < r->data) insertAt(r->left, x); else
-        insertAt(r->right, x);
+        if (r->data == x)
+            return; // Do nothing
+        if (x < r->data)
+            insertAt(r->left, x);
+        else
+            insertAt(r->right, x);
     }
-    Node* mRoot;
+    Node *mRoot;
     int mSize;
-
 };
 
 #endif // TREE_H_INCLUDED
