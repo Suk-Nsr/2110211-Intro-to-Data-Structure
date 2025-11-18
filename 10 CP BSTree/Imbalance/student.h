fe57c04f04753bb8a4b52c03ed0e376b916e5161
Sukrit Nuntasakulwiroj <sukrit.ntsw@gmail.com>
2025-11-18 19:28:11 +0700

    Imbalance nd try

--- 10 CP BSTree/Imbalance/student.h
+++ 10 CP BSTree/Imbalance/student.h
@@ -1,55 +1,37 @@
-int children(node *n)
+KeyT getValueOfMostImbalanceNode()
 {
-    if (n == NULL)
+    KeyT value;
+    int maxImbalance = -1;
+    findMostImbalance(this->mRoot, value, maxImbalance);
+    return value;
+}
+
+int findDepth(node *n)
+{
+    if (n == nullptr)
     {
         return -1;
     }
-    if (n->left == NULL && n->right == NULL)
-    {
-        return 0;
-    }
-    if (n->left == NULL)
-    {
-        return children(n->right) + 1;
-    }
-    if (n->right == NULL)
-    {
-        return children(n->left) + 1;
-    }
-    return std::max(children(n->left) + 1, children(n->right) + 1);
+    return std::max(findDepth(n->left) + 1, findDepth(n->right) + 1);
 }
 
 void findMostImbalance(node *n, KeyT &value, int &maxImbalance)
 {
-    if (n == NULL)
+    if (n == nullptr)
     {
         return;
     }
 
-    int left_height, right_height, imbalance;
-    left_height = children(n->left);
-    right_height = children(n->right);
-    imbalance = std::abs(left_height - right_height);
+    int leftHeight = findDepth(n->left);
+    int rightHeight = findDepth(n->right);
+    int imbalanceValue = std::abs(leftHeight - rightHeight);
 
-    if (imbalance > maxImbalance)
+    if (imbalanceValue > maxImbalance)
     {
-        maxImbalance = imbalance;
-        value = n->data.first;
-    }
-
-    if (imbalance == maxImbalance && mLess(n->data.first, value))
-    {
+        maxImbalance = imbalanceValue;
         value = n->data.first;
     }
 
     findMostImbalance(n->left, value, maxImbalance);
     findMostImbalance(n->right, value, maxImbalance);
-}
-
-KeyT getValueOfMostImbalanceNode()
-{
-    KeyT value;
-    int maxImbalance = -1;
-    findMostImbalance(mRoot, value, maxImbalance);
-    return value;
 }
