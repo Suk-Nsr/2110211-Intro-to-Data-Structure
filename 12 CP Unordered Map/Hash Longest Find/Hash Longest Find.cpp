6562c9ec2d79188a732ae54c7d961d95bb0c70ae
Sukrit Nuntasakulwiroj <sukrit.ntsw@gmail.com>
2025-12-02 00:10:23 +0700

    Hash Longest Find (Unfinished)

---
+++ 12 CP Unordered Map/Hash Longest Find/Hash Longest Find.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+    int n, m;
+    std::cin >> n >> m;
+
+    for (int i = 0; i < m; ++i)
+    {
+        int a, b;
+        std::cin >> a >> b;
+        if (a == 1)
+        {
+
+        }
+        if (a == 2)
+        {
+            
+        }
+    }
+}
